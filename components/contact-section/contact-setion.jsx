a553f88788e66a6526abc4797c8323f4abc2a666
krifalouay0@gmail.com <krifalouay0@gmail.com>
2024-04-28 17:04:33 +0100

    cppp

---
+++ components/contact-section/contact-setion.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+import Form from './Form'
+import Info from './info'
+
+import './contact-section.css'
+
+const ContactSection = () => (
+  <>
+    <div className="contact-section">
+      <Form />
+      <Info />
+    </div>
+  </>
+)
+
+export default ContactSection
